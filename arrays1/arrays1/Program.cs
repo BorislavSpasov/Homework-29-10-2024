67dfc833c3e364374b45142c09edaf61a04ad685
BorislavSpasov <borislav.e.spasov@gmail.com>
2024-11-11 18:35:47 +0200

    Add files via upload

---
+++ arrays1/arrays1/Program.cs
@@ -0,0 +1,7 @@
+﻿int[] array = new int[20];
+
+for (int i = 0; i < 20; i++)
+{
+    array[i] = i * 5;
+    Console.Write(array[i] + ", ");
+}
---
+++ arrays2/arrays2/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("Lenght of first array: ");
+int firstArrayLenght = int.Parse(Console.ReadLine());
+Console.Write("Lenght of second array: ");
+int secondArrayLenght = int.Parse(Console.ReadLine());
+
+if (firstArrayLenght != secondArrayLenght)
+{
+    Console.WriteLine("The arrays are not equal!");
+    return;
+}
+
+int[] firstArray = new int[firstArrayLenght];
+int[] secondArray = new int[secondArrayLenght];
+
+
+for (int i = 0; i < firstArrayLenght; i++)
+{
+    Console.Write($"Index {i} in the first array: ");
+    firstArray[i] = int.Parse(Console.ReadLine());
+
+}
+for (int i = 0; i < firstArrayLenght; i++)
+{
+    Console.Write($"Index {i} in the second array: ");
+    secondArray[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < firstArrayLenght; i++)
+{
+    if(firstArray[i] != secondArray[i])
+    {
+        Console.WriteLine("The arrays are not equal!");
+        return;
+    }
+}
+
+Console.WriteLine("The arrays are equal!");
